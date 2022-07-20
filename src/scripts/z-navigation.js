a85028b0fda9b43b376da0b7ca033691987e65e6
ArkadiusKuna <kuna.arek@gmail.com>
2022-07-20 10:23:42 +0200

    test commit

---
+++ src/scripts/z-navigation.js
@@ -0,0 +1,10 @@
+// const naviBtn = document.getElementById('naviBtn');
+// const naviItems = document.querySelectorAll(".mobile-nav__item");
+
+// naviItems.forEach((item) => {
+//     item.addEventListener('click', )
+// }
+    
+// function closeNavi() {
+
+// }
